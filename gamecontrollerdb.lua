b3f090804fab97ee810f4de46d2277e9dc9b568d
TsT <tst2005@gmail.com>
2015-02-09 23:50:51 +0100

    bugfix

--- gamecontrollerdb.lua
+++ gamecontrollerdb.lua
@@ -117,6 +117,8 @@ xinput,X360 Controller,a:b10,b:b11,back:b5,dpdown:b1,dpleft:b2,dpright:b3,dpup:b
 030000006f0e00001e01000011010000,Rock Candy Gamepad for PS3,platform:Linux,a:b1,b:b2,x:b0,y:b3,back:b8,start:b9,guide:b12,leftshoulder:b4,rightshoulder:b5,leftstick:b10,rightstick:b11,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:b6,righttrigger:b7,dpup:h0.1,dpleft:h0.8,dpdown:h0.4,dpright:h0.2,
 ]]
 
+require("love.joystick")
+local love = require("love")
 if love.joystick.loadGamepadMappings then love.joystick.loadGamepadMappings(mappings) end
 
 return _M
